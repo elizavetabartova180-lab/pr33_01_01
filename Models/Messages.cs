9b2c9204aa982685a1b39e1764b0418f2ce2e871
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-27 09:18:19 +0500

    Разработанные модели. Бартова

---
+++ Models/Messages.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Bartova.Models
+{
+    public class Messages
+    {
+        public int Id { get; set; }
+        public int UserFrom {  get; set; }
+        public int UserTo { get; set; }
+        public string Message { get; set; }
+        public Messages(int UserFrom, int UserTo, string Message) { 
+            this.UserFrom = UserFrom;
+            this.UserTo = UserTo;
+            this.Message = Message;
+        }
+    }
+}
---
+++ Models/Users.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Bartova.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Lastname { get; set; }
+        public string Firstname { get; set; }
+        public string Surname { get; set; }
+        public byte[] Photo { get; set; }
+        public Users(string Lastname, string Firstname, string Surname, byte[] Photo)
+        {
+            this.Lastname = Lastname;
+            this.Firstname = Firstname;
+            this.Surname = Surname;
+            this.Photo = Photo;
+        }
+        public string ToFIO()
+        {
+            return $"{Lastname} {Firstname} {Surname}";
+        }
+    }
+}
