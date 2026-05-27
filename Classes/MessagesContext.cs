4458308212ef343a4cf06fcaaef7fd95ea7d1f55
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-27 09:32:04 +0500

    Разработанные контексты данных. Бартова

---
+++ Classes/MessagesContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ChatStudents_Bartova.Classes.Common;
+using ChatStudents_Bartova.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Bartova.Classes
+{
+    public class MessagesContext : DbContext
+    {
+        public DbSet<Messages> Messages { get; set; }
+        public MessagesContext() =>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) =>
+            optionsBuilder.UseSqlServer(Config.config);
+    }
+}
---
+++ Classes/UsersContext.cs
@@ -0,0 +1,16 @@
+﻿using ChatStudents_Bartova.Classes.Common;
+using ChatStudents_Bartova.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ChatStudents_Bartova.Classes
+{
+    public class UsersContext: DbContext
+    {
+        public DbSet<Users> Users { get; set; }
+        public UsersContext()=>
+            Database.EnsureCreated();
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)=>
+            optionsBuilder.UseSqlServer(Config.config);
+        
+    }
+}
