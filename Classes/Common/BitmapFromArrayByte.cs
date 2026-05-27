efb3621bf469e51f5a11248d64d6cefebea3bd08
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-27 09:26:39 +0500

    Разработанные общие классы. Бартова

---
+++ Classes/Common/BitmapFromArrayByte.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+using System.Windows.Media.Imaging;
+
+namespace ChatStudents_Bartova.Classes.Common
+{
+    public class BitmapFromArrayByte
+    {
+        public static BitmapImage LoadImage(byte[] imageData)
+        {
+            BitmapImage Image = new BitmapImage();
+            using(var Steam = new MemoryStream(imageData))
+            {
+                Steam.Position = 0;
+                Image.BeginInit();
+                Image.CreateOptions = BitmapCreateOptions.PreservePixelFormat;
+                Image.CacheOption = BitmapCacheOption.OnLoad;
+                Image.UriSource= null;
+                Image.StreamSource= Steam;
+                Image.EndInit();
+            }
+            Image.Freeze();
+            return Image;
+        }
+    }
+}
---
+++ Classes/Common/Config.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChatStudents_Bartova.Classes.Common
+{
+    public class Config
+    {
+        public static readonly string config = "Server=;" +
+            "Trusted_Connection = False;" +
+            "Database=***;" +
+            "User=***;" +
+            "Pwd=***;"
+    }
+}
