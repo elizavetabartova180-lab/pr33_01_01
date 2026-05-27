66ad9be5d4eab0c48fc301bbf13a7f0251c40021
Kik02 <Golovin.Savelii@yandex.ru>
2026-05-27 10:00:54 +0500

    шаг20

---
+++ Pages/Login.xaml.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using ChatStudents_Bartova.Classes;
+using Microsoft.Win32;
+
+namespace ChatStudents_Bartova.Pages
+{
+    /// <summary>
+    /// Логика взаимодействия для Login.xaml
+    /// </summary>
+    public partial class Login : Page
+    {
+        public string srcUserImage = "";
+        UsersContext usersContext = new UsersContext();
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void SelectPhoto(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Title = "Выберите фотографию:";
+            openFileDialog.InitialDirectory = @"C:\";
+            openFileDialog.Filter = "JPF Filter (*.jpg)|*.jpg|PNG Filter (*.png)|*.png|All filter (*.*)|*.*";
+            if(openFileDialog.ShowDialog()==true)
+            {
+                imgUser.Source = new BitmapImage(new Uri(openFileDialog.FileName));
+                srcUserImage = openFileDialog.FileName;
+            }
+        }
+        public bool CheckEmpty(string Pattern, string Input)
+        {
+            Match m = Regex.Match(Input, Pattern);
+            return m.Success;
+        }
+
+        private void Continue(object sender, RoutedEventArgs e)
+        {
+            if (!CheckEmpty("^[А-ЯёЁ][а-яА-ЯёЁ]*$", Lastname.Text))
+            {
+                MessageBox.Show("Укажите фамилию.");
+                return;
+            }
+            if (!CheckEmpty("^[А-ЯёЁ][а-яА-ЯёЁ]*$", Firstname.Text))
+            {
+                MessageBox.Show("Укажите имя.");
+                return;
+            }
+            if (!CheckEmpty("^[А-ЯёЁ][а-яА-ЯёЁ]*$", Surname.Text))
+            {
+                MessageBox.Show("Укажите отчество.");
+                return;
+            }
+            if (String.IsNullOrEmpty(srcUserImage))
+            {
+                MessageBox.Show("Выберите изображение.");
+                return;
+            }
+            //доделать
+        }
+    }
+}
